f88b43ce29018a811987c473f5802ce1d75be81f
Chen Kasirer <ckasirer@ethz.ch>
2024-09-02 17:31:48 +0200

    zeroed out version

--- src/installlib/__init__.py
+++ src/installlib/__init__.py
@@ -18,18 +18,16 @@ import os
 
 
 __author__ = ["Chen Kasirer"]
-__copyright__ = ""
+__copyright__ = "Gramazio Kohler Research, ETH Zurich"
 __license__ = "MIT License"
 __email__ = "kasirer@arch.ethz.ch"
-__version__ = "0.1.0"
+__version__ = "0.0.0"
 
 
 HERE = os.path.dirname(__file__)
 
 HOME = os.path.abspath(os.path.join(HERE, "../../"))
-DATA = os.path.abspath(os.path.join(HOME, "data"))
 DOCS = os.path.abspath(os.path.join(HOME, "docs"))
-TEMP = os.path.abspath(os.path.join(HOME, "temp"))
 
 
-__all__ = ["HOME", "DATA", "DOCS", "TEMP"]
+__all__ = ["HOME", "DOCS"]
