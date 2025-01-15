d274bce319f3057bf5d2e9be0c5d91eb482d0d96
Chen Kasirer <ckasirer@ethz.ch>
2025-01-15 14:12:28 +0100

    Bump version to 0.3.0

--- src/installlib/__init__.py
+++ src/installlib/__init__.py
@@ -21,7 +21,7 @@ __author__ = ["Chen Kasirer"]
 __copyright__ = "Gramazio Kohler Research, ETH Zurich"
 __license__ = "MIT License"
 __email__ = "kasirer@arch.ethz.ch"
-__version__ = "0.2.3"
+__version__ = "0.3.0"
 
 
 HERE = os.path.dirname(__file__)
